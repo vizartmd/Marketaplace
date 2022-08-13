c4282f2ae6b1b8fabf66727e02ff70644342b85a
Robu Victor <vizart.md@gmail.com>
2022-08-13 17:34:58 +0300

    added ANT_PATH_MATCHER in application.properties

--- src/main/java/com/marketplace/MarketplaceRestApiApplication.java
+++ src/main/java/com/marketplace/MarketplaceRestApiApplication.java
@@ -4,9 +4,13 @@ import com.marketplace.model.dto.MySQLCredentials;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication(scanBasePackages = "com.marketplace")
+@EnableWebMvc
+@EnableSwagger2
 @EnableJpaAuditing
+@SpringBootApplication(scanBasePackages = "com.marketplace")
 public class MarketplaceRestApiApplication {
 
 	public static void main(String[] args) {
@@ -24,3 +28,4 @@ public class MarketplaceRestApiApplication {
 		SpringApplication.run(MarketplaceRestApiApplication.class, args);
 	}
 }
+
